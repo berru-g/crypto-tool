97cf6308a087a233d3639e3bde47b3c3a28e45f9
berru-g <g.leberruyer@gmail.com>
2025-02-22 01:27:06 +0100

    button app no fonctionel

--- readme.md
+++ readme.md
@@ -4,9 +4,11 @@
 
 -   ✅ Si vous souhaitez une [explication sur cette outil](https://medium.com/@gael-berru/fibonacci-retracement-a56d53d5863d).
 -   ✅ Si vous êtes débutant, [comment choisir un bon token ?](https://savoir-relatif-et-absolu.netlify.app/comment-choisir-un-bon-token)
--   🪙 Si cet outil vous est utile est que [vous souhaitez faire un don](https://savoir-relatif-et-absolu.netlify.app/donation) 😉
+-   🪙 Si cet outil vous est utile est que [vous souhaitez faire un don](https://savoir-relatif-et-absolu.netlify.app/donation) 
 -   ⚙️ ou [une remarque pour une amélioration](https://savoir-relatif-et-absolu.netlify.app/index.html/#espace-commentaire) du tool.
 
+### App à installer depuis edge ou chrome, firefox ne prends pas en charge.
 
+<img width="402" alt="Capture d'écran_20250222_010520" src="https://github.com/user-attachments/assets/52213122-849f-4cb3-9779-04d95263dac2" />
 
 ##### MIT License with Non-Commercial Clause
--- sw.js
+++ sw.js
@@ -1,23 +1,35 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('crypto-tool-cache').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/new.css',
-          '/new.js',
-          '/img',
-          // Ajoute ici les autres ressources à mettre en cache
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('crypto-tool-cache').then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/new.css',
+        '/new.js',
+        '/img',
+        // Ajoute ici les autres ressources à mettre en cache
+      ]);
+    })
+  );
+});
+/*
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installé !");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activé !");
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("Intercepté :", event.request.url);
+});
+*/
